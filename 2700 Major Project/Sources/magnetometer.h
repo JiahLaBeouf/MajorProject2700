634e62f2df022879a5eb3549699c6cec811c0cc5
LachMiz <lachlan.mizon@gmail.com>
2022-05-19 22:17:41 +1000

    Main is unfinished and doesn't compile lol
    Pushing for the sake of saving

--- 2700 Major Project/Sources/magnetometer.h
+++ 2700 Major Project/Sources/magnetometer.h
@@ -2,11 +2,5 @@
 #define MAGNETOMETER_HEADER
 
 
-// data structures containing the raw values
-typedef struct MagRaw {
-  int x;
-  int y;
-  int z;
-} MagRaw;
 
 #endif
--- 2700 Major Project/Sources/main.c
+++ 2700 Major Project/Sources/main.c
@@ -1,6 +1,9 @@
 #include <hidef.h>      /* common defines and macros */
 #include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
 #include "derivative.h"      /* derivative-specific definitions */
 
 #include "motorMovement.h" /*Moving the motor functions*/
@@ -9,23 +12,74 @@
 #include "laser.h"
 #include "magnetometer.h"
 
+void delay(void)
+{
+  int j;
+  for(j=1;j<=1201923;j++){
+  }
+        
+}
 
 void main(void) {
   /* put your own code here */
   
-  MagRaw read_magnet;
-  getRawDataMagnet(&read_magnet);
   
+  /*IDENTIFYING MAGNET CODE*/
+  //Variable storing the largest magnet value corrosponding towards pointing towards the magnet & position of servo
+  int maxMagValue = 0;
+  int maxPosition =0;
+  int i;
+  
+  unsigned long laserDistance;
+  unsigned long cartonLength = 0.098;
+  int milkNum;
+  //Initialise magnet struct
+  MagRaw read_magnet;
+  
+  //Turn on PWM and turn off cop
   PWMinitialise;
-  setHorizontal(90);
+  laserInit();
+  EnableInterrupts;
+  _DISABLE_COP();
+   
+  //Set the motors back to starting position
+  setHorizontal(0);
   setVertical(90);
+  
+  //A for loop that cycles through 180 degrees slowly, recording the highest magnetometer value
+  for(i=1;i<=180;i++){
+  
+    //Increment the horizontal angle by 1 degree 
+    setHorizontal(i);
+  
+    //Read the magnet data
+    getRawDataMagnet(&read_magnet);
+  
+    if(read_magnet.x>maxMagValue){
+  
+      //Store t
+      maxMagValue = read_magnet.x;
+      maxPosition = i;
+    }
+  
+    //Delay for 0.05sec
+    delay();
+  }
+  
+  //Set the servo to the position with the highest recorded value
+  setHorizontal(maxPosition);
+  
+  
+  /*MEASURING MILK CARTONS CODE*/
+  GetLatestLaserSample(&laserDistance);
+  
 
-
-	EnableInterrupts;
-
-
+  
+  milkNum = round((laserDistance-0.01)/cartonLength);
+  
   for(;;) {
     _FEED_COP(); /* feeds the dog */
   } /* loop forever */
   /* please make sure that you never leave main */
 }
+
